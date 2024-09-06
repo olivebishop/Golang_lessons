26d526745364a50fac38baa521a422d8528005e7
olivebishop <olivehendrilgen1@gmail.com>
2024-09-06 13:04:33 +0300

    feat: lesson3_condtions_switch

---
+++ lessons/lesson_three/conditions/loops/loops.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+
+
+// we create loop usiing 'for'.create a varable x and assign to number 1 , if x is less or equal to 1 run the loop and end of loop add x and it ends at 10
+for x := 1; x <= 10; x++ {
+	fmt.Println(x)
+
+}
+
+//or
+m:=1
+
+for m <=10{
+	fmt.Println(m)
+}
+
+
+
+}
