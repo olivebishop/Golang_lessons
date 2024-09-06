606ae4e04b3dd04bec3b7851e3afaa3fff8608e3
olivebishop <olivehendrilgen1@gmail.com>
2024-09-06 13:03:34 +0300

    feat: lesson3_condtions_ifElse

---
+++ lessons/lesson_three/conditions/if_else/if-else.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func main(){
+	age:= 23
+    
+	if age >= 23 {
+		fmt.Println(" you are allowed to the party boss")	
+		}else{
+			fmt.Println("chill mate we check on your ID!")
+		}
+
+		if age >=20{
+			fmt.Println("three more years boss to be invied")
+		}else{
+			fmt.Println("fuck off the strip club area !")
+		}
+
+	}
+
